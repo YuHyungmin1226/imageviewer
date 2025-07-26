99b1aaf54e298c1da83de6fb46aba0c998761316
YHM <dew1052ii@gmail.com>
2025-07-26 14:14:10 +0900

    Remove personal_sns_app project

--- personal_sns_app/supabase_minimal_update.sql
+++
@@ -1,8 +0,0 @@
--- 최소 명령어로 URL 미리보기 기능 활성화
--- Supabase SQL 편집기에서 실행
-
--- url_previews 컬럼 추가 (이미 있으면 오류 무시)
-ALTER TABLE posts ADD COLUMN IF NOT EXISTS url_previews JSONB DEFAULT '[]';
-
--- 기존 데이터 초기화
-UPDATE posts SET url_previews = '[]' WHERE url_previews IS NULL; 
